720f27d03604bbda74ac5191608010e528b6044c
drewseabase <143631824+drewseabase@users.noreply.github.com>
2025-12-16 22:27:07 -0500

    lots of practice today

--- bits_practice.py
+++ bits_practice.py
@@ -1,11 +1,12 @@
-integer = int(input("Enter an int "))
-binary = bin(integer)
-binary_count = 0
+name = input("What is your name? ")
+age = int(input("Enter your age: "))
+random_num = int(input("Enter a random number: "))
+current_year = 2025
+number = 0
 
-for i in binary:
-    if i == '1':
-        binary_count+=1
-print(binary_count)
+calc_year = (current_year - age) + 100
 
-
+while random_num != number:
+    print(f'Hi {name}, you are currently {age} and you will turn 100 in the year {calc_year}')
+    number += 1
 
--- find_outlier.py
+++ find_outlier.py
@@ -1,2 +1,19 @@
-entered_int = int(input("Enter an integer "))
+entered_string = input("Enter an series of numbers ")
+split_string = entered_string.split()
+
+highest = int(split_string[0])
+lowest = int(split_string[0])
+
+for i in split_string[1:]:
+    num = int(i)
+    if num >= highest:
+        highest = num
+    elif num <= lowest:
+        lowest = num
+
+print(f'{str(highest)} {str(lowest)}')
+
+
+
+
 
